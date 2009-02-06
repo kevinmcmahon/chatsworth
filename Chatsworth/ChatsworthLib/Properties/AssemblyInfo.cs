16540bbee62fe3d6478101d04fff0916309ecaf1
Kevin McMahon <chatsworth@kevfoo.com>
2009-02-06 00:07:48 +0000

    Rev assembly versions for new release.

--- Chatsworth/ChatsworthLib/Properties/AssemblyInfo.cs
+++ Chatsworth/ChatsworthLib/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ChatsworthLib")]
-[assembly: AssemblyDescription("Google Talk Chatbot Library")]
+[assembly: AssemblyDescription("Core Library for Chatsworth")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kevin McMahon")]
 [assembly: AssemblyProduct("ChatsworthLib")]
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.2")]
-[assembly: AssemblyFileVersion("0.0.0.2")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
 [assembly: log4net.Config.XmlConfigurator
   (ConfigFile = "log4net.config", Watch = true)]
